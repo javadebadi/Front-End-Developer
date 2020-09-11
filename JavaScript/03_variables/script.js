fa4586d9bb94fb76f15bf3a2d3ceb5a453b83e07
javadebadi <javad.ebadi.1990@gmail.com>
2020-09-11 01:38:24 -0700

    Add: added example of variables in javascript (number, string, object, boolean, undefined)

---
+++ JavaScript/03_variables/script.js
@@ -0,0 +1,13 @@
+var integer_var = 2;
+var double_var = 2.36567;
+var string_var = "Javad Ebadi";
+var null_var = null;
+var boolean_var = true;
+var undefined_var;
+
+console.log("Type of interger_var: " + typeof integer_var);
+console.log("Type of double_var: " + typeof double_var);
+console.log("Type of string_var: " + typeof string_var);
+console.log("Type of null_var: "+ typeof null_var);
+console.log("Type of boolean_var: " + typeof boolean_var);
+console.log("Type of undefined_var: " + typeof undefined_var);
