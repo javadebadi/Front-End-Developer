b5e77fedc90fe8a68ba1784258d258b3bd3efe2a
javadebadi <javad.ebadi.1990@gmail.com>
2020-09-16 14:29:47 -0700

    Update: added some new comments

--- JavaScript/07_functions_anonymous/script.js
+++ JavaScript/07_functions_anonymous/script.js
@@ -1,4 +1,12 @@
 // Anonymous functions
+/* Anonymous functions are not hoisted:
+They will be assigned to a variable therefore they will not be
+created until javascripts reaches to them in run time.
+However for named functions (function declaration) the javascript
+will hoist the function declaration and the function will be
+defined even before javascript reach to that fucntion use
+in run */
+
 var theMultiplication = function(a,b){
   return a*b;
 }
