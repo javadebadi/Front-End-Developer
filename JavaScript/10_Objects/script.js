ec72b7036ab72e883e4037ce05120a62c226bf6a
javadebadi <javad.ebadi.1990@gmail.com>
2020-09-19 15:00:59 -0700

    Update: added example of bracket notation to access attributes of object in javascript

--- JavaScript/10_Objects/script.js
+++ JavaScript/10_Objects/script.js
@@ -3,6 +3,7 @@ var human = {
   name: "Javad Ebadi",
   age: 30,
   sex: "male",
+  "national-ID":1133300022,
   increaseAge: function(){
     return human.age++;
   }
@@ -13,3 +14,10 @@ var human = {
 console.log("age: "+human.age)
 human.increaseAge()
 console.log("age: "+human.age)
+
+
+// we can use bracket notation to access attributes and
+// methods of object
+// especially when the name of attibute is not appropriatly chosen
+console.log("sex: "+human["sex"])
+console.log("national-ID: "+human["national-ID"])
